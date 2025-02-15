a13ac45c26f61b289951a8471f6779f7ef806381
Lucas Policene <lucasantoniopolicene4@gmail.com>
2025-02-15 18:09:27 -0300

    Documentação adicionada

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function carregar(){
-var msg = window.document.getElementById('msg')
+var msg = window.document.getElementById('msg') //ligação das div com o javascript utilizando os id
 var img = window.document.getElementById('imagem')
 var data = new Date()
 var hora = data.getHours()
@@ -7,7 +7,7 @@ msg.innerHTML = `Agora são ${hora} horas`
 if(hora >= 0 && hora < 12){
 
     img.src = 'img3.png'
-    document.body.style.backgroundColor ='#e2cd9f'
+    document.body.style.backgroundColor ='#e2cd9f' // comando para mudar a cor do fundo para cada img
 
 }else if(hora >= 12 && hora <=18){
 
